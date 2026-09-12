811413d46f8950531f088d01c1d155e64d5fe785
Poci01 <luthfimaulana1994@gmail.com>
2026-09-12 15:30:55 +0700

    fix: relocate icons export to src/components/icons/index.tsx

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1,25 +1,27 @@
+import React from "react";
 import {
-  LucideProps,
+  LayoutDashboard,
+  FileText,
+  HelpCircle,
+  BarChart2,
+  Settings,
+  Menu,
+  Search,
+  Sun,
+  Moon,
   BookOpen,
   Users,
   CheckCircle,
-  FileText,
   Clock,
   Award,
-  BarChart2,
-  Settings,
-  Bell,
   LogOut,
   Plus,
   Trash2,
   Edit,
   Upload,
   Download,
-  Search,
   ChevronRight,
   ChevronLeft,
-  Sun,
-  Moon,
   Shield,
   User,
   AlertCircle,
@@ -28,11 +30,28 @@ import {
   Eye,
   ArrowRight,
   Sparkles,
-  HelpCircle,
   FileUp,
-  BrainCircuit
+  BrainCircuit,
+  Bell
 } from "lucide-react";
 
+// Named Exports khusus yang dipanggil oleh AdminApp & UI komponen lainnya
+export const AppLogo = (props: React.SVGProps<SVGSVGElement>) => <BookOpen className="w-6 h-6 text-blue-600" {...props} />;
+export const IcDashboard = LayoutDashboard;
+export const IcFileText = FileText;
+export const IcHelp = HelpCircle;
+export const IcBar = BarChart2;
+export const IcSettings = Settings;
+export const IcMenu = Menu;
+export const IcSearch = Search;
+export const IcSun = Sun;
+export const IcMoon = Moon;
+export const IcBell = Bell;
+export const IcUser = User;
+export const IcShield = Shield;
+export const IcLogout = LogOut;
+
+// Named Exports Objek Icons Universal
 export const Icons = {
   BookOpen,
   Users,
